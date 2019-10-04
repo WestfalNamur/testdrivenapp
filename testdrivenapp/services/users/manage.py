d3eada2e07f3950a4bd1732f7d04d8baea14f853
Stefan Crummenerl <crummenerls@gmail.com>
2019-10-04 14:00:03 +0200

    Add code coverage and quality

--- testdrivenapp/services/users/manage.py
+++ testdrivenapp/services/users/manage.py
@@ -4,16 +4,24 @@
 import sys
 import unittest
 
+import coverage
 from flask.cli import FlaskGroup
 
 from project import create_app, db
-from project.api.models import User  
-
-app = create_app()  
-cli = FlaskGroup(create_app=create_app)  
+from project.api.models import User
 
 
-@cli.command('recreate_db')
+COV = coverage.coverage(
+    branch=True, include="project/*", omit=["project/tests/*", "project/config.py"]
+)
+COV.start()
+
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
+
+@cli.command("recreate_db")
 def recreate_db():
     db.drop_all()
     db.create_all()
@@ -23,20 +31,36 @@ def recreate_db():
 @cli.command()
 def test():
     """Runs the tests without code coverage"""
-    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    tests = unittest.TestLoader().discover("project/tests", pattern="test*.py")
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
     sys.exit(result)
 
 
-@cli.command('seed_db')
+@cli.command("seed_db")
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(User(username="michael", email="hermanmu@gmail.com"))
+    db.session.add(User(username="michaelherman", email="michael@mherman.org"))
     db.session.commit()
 
 
-if __name__ == '__main__':
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover("project/tests")
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print("Coverage Summary:")
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
+
+
+if __name__ == "__main__":
     cli()
