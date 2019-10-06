26e5d5ded31d333a20bc815072093c1e7942d1e2
Stefan Crummenerl <crummenerls@gmail.com>
2019-10-06 14:59:44 +0200

    Add userinput form

--- testdrivenapp/services/client/src/index.js
+++ testdrivenapp/services/client/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 import axios from "axios";
 
 import UsersList from "./components/UsersList";
+import AddUser from "./components/AddUser";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: "",
+      email: ""
     };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -27,16 +32,35 @@ class App extends Component {
       });
   }
 
+  addUser(event) {
+    event.preventDefault();
+    console.log("sanity check!");
+    console.log(this.state);
+  }
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
   render() {
-    console.log(this.state.users);
     return (
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
               <br />
               <h1 className="title is-1">All Users</h1>
               <hr />
+              <br />
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
+              <br />
               <br />
               <UsersList users={this.state.users} />
             </div>
