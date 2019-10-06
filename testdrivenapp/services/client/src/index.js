781ff073a3f9682555d4a4b18b22e22a4bf94233
Stefan Crummenerl <crummenerls@gmail.com>
2019-10-06 16:13:53 +0200

    Deploy on AWS

--- testdrivenapp/services/client/src/index.js
+++ testdrivenapp/services/client/src/index.js
@@ -42,7 +42,8 @@ class App extends Component {
     axios
       .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
       .then(res => {
-        this.getUsers();        this.setState({ username: "", email: "" });
+        this.getUsers();
+        this.setState({ username: "", email: "" });
       })
       .catch(err => {
         console.log(err);
