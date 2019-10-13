e5cc48ec7e8c3ed8872225680d4d1e3479eb8845
Stefan Crummenerl <crummenerls@gmail.com>
2019-10-13 15:39:56 +0200

    ENV-VARIABLES from file

--- testdrivenapp/services/client/src/App.js
+++ testdrivenapp/services/client/src/App.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { Route, Switch } from "react-router-dom";
 
 import UsersList from "./components/UsersList";
-import AddUser from "./components/AddUser";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import Form from "./components/Form";
